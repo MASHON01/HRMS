85678e9489b11f1659cb4c1557185c2307370580
Mashon01 <ntutupeter1@gmail.com>
2022-03-19 15:26:51 +0300

    First commit

---
+++ Human_Resource_Management_System_Django/hrms/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'users'
+
+
+class HrmsConfig(AppConfig):
+    name = 'hrms'
